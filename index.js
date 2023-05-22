d747ea625841430030613d976965d81e3b763a64
sfshovon <shahriar.shovon@northsouth.edu>
2023-05-23 05:18:20 +0600

    Modified Vercel Config 6

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const connectDB = require("./config/connectDB");
 const expensesRouter = require("./routes/expenseRoutes");
 require('dotenv').config();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
