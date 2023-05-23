175de907d7fe7c9353510915ee533abaa509f0db
sfshovon <shahriar.shovon@northsouth.edu>
2023-05-23 21:03:20 +0600

    Modified Index.js And DB Connection

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use("/expense", expenseRouter)
+app.use("/expenseTracker", expenseRouter)
 
 app.get("/", (req, res)  => {
   res.send("Expense Tracking App's server is running");
