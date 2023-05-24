4494390fc45e390328a32462a4e29b0c1ffa49fd
sfshovon <shahriar.shovon@northsouth.edu>
2023-05-24 13:43:22 +0600

    Vercel Deployment Issue Solved

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use("/expenseTracker", expenseRouter)
+app.use("/expenseRecord", expenseRouter)
 
 connectDB().catch(console.dir);
 
